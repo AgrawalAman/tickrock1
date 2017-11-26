c20f8b64088f3889c5cc8f565f5ba3b2f69f290b
Aman Agrawal <aa.in201@gmail.com>
2017-11-26 17:01:47 -0500

    Added gif functionality

--- game.js
+++ game.js
@@ -1,22 +1,26 @@
-function start() {
 
-    var time = Math.floor((Math.random() * 100) + 200);
-    var karma = Math.floor((Math.random() * 100) + 10);;
-    document.getElementById("karma").innerHTML = "Karma: " + karma;
-    document.getElementById("time").innerHTML = "Time: " + time + "s";
-    var counter=setInterval(timer, 1000);
-
-    function timer()
-    {
-      time=time-1;
-      if (time <= 0)
-      {
-         clearInterval(counter);
-         //counter ends, do end game alert over here
-         return;
-      }
-      document.getElementById("time").innerHTML = "Time: " + time + "s";;
-    }
-
+function volunteer() {
+  document.getElementById('gamegif').src = "volunteer.gif"
 }
-window.onload = start;
+
+function plant() {
+  document.getElementById('gamegif').src = "plant.gif"
+}
+function drink() {
+  document.getElementById('gamegif').src = "drink.gif"
+}
+
+function playmusic() {
+  document.getElementById('gamegif').src = "volunteer.gif"
+}
+
+function vacation() {
+  document.getElementById('gamegif').src = "vacation.gif"
+}
+function car() {
+  document.getElementById('gamegif').src = "car.gif"
+}
+
+function movie() {
+  document.getElementById('gamegif').src = "movie.gif"
+}
