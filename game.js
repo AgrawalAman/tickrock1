50b56b4a5b6706f59611c11a137b915f9365f5a4
Aman Agrawal <aa.in201@gmail.com>
2017-12-04 06:39:58 -0500

    working fine

--- game.js
+++ game.js
@@ -2,72 +2,73 @@ var time;
 var karma;
 
 function start() {
-  time = Math.floor((Math.random() * 100) + 200);
-  karma = Math.floor((Math.random() * 100) + 10);
+  time = Math.floor((Math.random() * 100) + 60);
+  karma = Math.floor((Math.random() * 100) + 50);
   document.getElementById("karma").innerHTML = "Karma: " + karma;
   document.getElementById("time").innerHTML = "Time: " + time + "s";
+  document.getElementById('volunteer').addEventListener("click",volunteer);
+  document.getElementById('plant').addEventListener("click",plant);
+  document.getElementById('car').addEventListener("click",car);
+  document.getElementById('vacation').addEventListener("click",vacation);
+  document.getElementById('fight').addEventListener("click",fight);
+  document.getElementById('trade').addEventListener("click",trade);
 
   var counter=setInterval(timer, 1000);
   function timer()
     {
       time=time-1;
-      if (time <= 0)
+      if (time <= 0 || karma <=0)
       {
          clearInterval(counter);
          document.getElementById("gamegif").src = "images/gameover.gif"
          window.alert("You are dead!");
          return;
       }
+      
       document.getElementById("time").innerHTML = "Time: " + time + "s";
       document.getElementById("karma").innerHTML = "Karma: " + karma;
 
     }
 
-    document.getElementById('volunteer').addEventListener("click",volunteer);
-    document.getElementById('plant').addEventListener("click",plant);
-    document.getElementById('car').addEventListener("click",car);
-    document.getElementById('vacation').addEventListener("click",vacation);
-    document.getElementById('fight').addEventListener("click",fight);
-    document.getElementById('trade').addEventListener("click",trade);
 
 }
 window.onload = start;
 
 function volunteer() {
   document.getElementById('gamegif').src = "images/volunteer.gif";
-  time += 100;
+  time -= 50;
   karma += 100;
   playSound();
 }
 
 function plant() {
   document.getElementById('gamegif').src = "images/plant.gif";
-  time += 100;
-  karma += 100;
+  time -= 100;
+  karma += 250;
   playSound();
 }
 function car() {
   document.getElementById('gamegif').src = "images/car.gif";
-  time += 100;
-  karma += 100;
+  time -= 300;
+  karma -= 1000;
   playSound();
 }
 function vacation() {
   document.getElementById('gamegif').src = "images/vacation.gif";
-  time += 100;
-  karma += 100;
+  time -= 1000;
+  karma -= 1000;
   playSound();
 }
 function fight() {
   document.getElementById('gamegif').src = "images/fight.gif";
-  time += 100;
-  karma += 100;
+  time += 300;
+  karma -= 100;
   playSound();
 }
 function trade() {
   document.getElementById('gamegif').src = "images/trade.gif";
   time += 100;
-  karma += 100;
+  karma -= 200;
   playSound();
 }
 
