2855faaa9faf68b30f6e31e1a96da1681d00dff1
Aman Agrawal <aa.in201@gmail.com>
2017-12-04 05:51:18 -0500

    added stuff

--- game.js
+++ game.js
@@ -2,13 +2,13 @@ var time;
 var karma;
 
 function start() {
-    time = Math.floor((Math.random() * 100) + 200);
-    karma = Math.floor((Math.random() * 100) + 10);;
-    document.getElementById("karma").innerHTML = "Karma: " + karma;
-    document.getElementById("time").innerHTML = "Time: " + time + "s";
-    var counter=setInterval(timer, 1000);
+  time = Math.floor((Math.random() * 100) + 200);
+  karma = Math.floor((Math.random() * 100) + 10);
+  document.getElementById("karma").innerHTML = "Karma: " + karma;
+  document.getElementById("time").innerHTML = "Time: " + time + "s";
 
-    function timer()
+  var counter=setInterval(timer, 1000);
+  function timer()
     {
       time=time-1;
       if (time <= 0)
@@ -19,33 +19,54 @@ function start() {
          return;
       }
       document.getElementById("time").innerHTML = "Time: " + time + "s";
+      document.getElementById("karma").innerHTML = "Karma: " + karma;
+
     }
 
+    document.getElementById('volunteer').addEventListener("click",volunteer);
+    document.getElementById('plant').addEventListener("click",plant);
+    document.getElementById('car').addEventListener("click",car);
+    document.getElementById('drink').addEventListener("click",drink);
+    document.getElementById('fight').addEventListener("click",fight);
+    document.getElementById('vacation').addEventListener("click",vacation);
+    document.getElementById('trade').addEventListener("click",trade);
+    document.getElementById('random').addEventListener("click",random);
+    document.getElementById('movie').addEventListener("click",movie);
 }
 window.onload = start;
 
 function volunteer() {
-  document.getElementById("gamegif").src = "images/volunteer.gif"
+  document.getElementById('gamegif').src = "images/volunteer.gif";
+  time += 100;
+  karma += 100;
 }
-
 function plant() {
-  document.getElementById('gamegif').src = "images/plant.gif"
+  document.getElementById('gamegif').src = "images/plant.gif";
+  time += 100;
+  karma += 100;
 }
 function drink() {
-  document.getElementById('gamegif').src = "images/drink.gif"
+  document.getElementById('gamegif').src = "images/drink.gif";
+  time += 100;
+  karma += 100;
 }
+function movie() {
+  document.getElementById('gamegif').src = "images/movie.gif";
+  time += 100;
+  karma += 100;
+}
+function car() {
+  document.getElementById('gamegif').src = "images/car.gif";
 
-function playmusic() {
-  document.getElementById('gamegif').src = "images/volunteer.gif"
+}
+function fight() {
+  document.getElementById('gamegif').src = "images/fight.gif";
 }
 
 function vacation() {
-  document.getElementById('gamegif').src = "images/vacation.gif"
-}
-function car() {
-  document.getElementById('gamegif').src = "images/car.gif"
+  document.getElementById('gamegif').src = "images/vacation.gif";
 }
 
-function movie() {
-  document.getElementById('gamegif').src = "images/movie.gif"
+function trade() {
+  document.getElementById('gamegif').src = "images/trade.gif";
 }
